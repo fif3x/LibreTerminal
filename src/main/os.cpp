2ad4c56f159c6f7457e8b53dbcd8f5a061f3fa3e
fif3x <185714407+fif3x@users.noreply.github.com>
2026-04-25 18:17:46 +0300

    Added implementation for the full_name_OS option in the config.

--- src/main/os.cpp
+++ src/main/os.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 #include "../../include/main/os.h"
+#include "../../include/main/configvars.h"
 
 
 void print_os()
 {
     os OS = detect_os();
-
+    
     if (OS == os::WIN)
-    {
-        std::cout << "WIN>";
+    {    
+        if(config::full_name_OS){
+            std::cout << "Windows>";
+        } else {
+            std::cout << "WIN>";
+        }
     }
     else if (OS == os::LINUX)
     {
-        std::cout << "LNX>";
+        if(config::full_name_OS){
+            std::cout << "Linux>";
+        } else {
+            std::cout << "LNX>";
+        }
     }
     else
     {
